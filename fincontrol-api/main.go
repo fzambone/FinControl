eb5fd59697a6357ad8b561c166929a95b7a9fd87
fzambone <fernando.zambone@gmail.com>
2023-12-26 22:33:10 -0300

    Integrated Fiber framework and GORM for DB

--- fincontrol-api/main.go
+++ fincontrol-api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
 
 func main() {
-	fmt.Println("Hello, FinControl!")
+	dsn := "user:password@tcp(db:3306)/fincontrol?charset=utf8mb4&parseTime=true&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!!")
+	}
+
+	fmt.Println("DB Connected")
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, FinControl with Fiber Server!")
+	})
+
+	app.Listen(":8080")
 }
